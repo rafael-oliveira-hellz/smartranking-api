2a9d5b543792a14abdc4e1a5fc4307bfb4a70407
Rafael de Oliveira <rafael.oliveirasylva@gmail.com>
2022-07-05 11:00:45 -0300

    Revert "+ First commit"
    This reverts commit 29fa3f190246efee3538b20e0d182686aefe6e27.

--- src/challenges/interfaces/challenge-status.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum ChallengeStatus {
-  DONE = 'REALIZADO',
-  PENDING = 'PENDENTE',
-  ACCEPTED = 'ACEITO',
-  DENIED = 'NEGADO',
-  CANCELED = 'CANCELADO'
-}
--- src/players/dtos/update-player.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class UpdatePlayerDto {
-  @IsNotEmpty()
-  readonly phoneNumber: string;
-
-  @IsNotEmpty()
-  readonly name: string;
-}
